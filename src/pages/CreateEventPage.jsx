41c294330529330df618e6dae9210a68f0197a30
MrNavapat <Mr.Navapat.Sujirachato@gmail.com>
2024-03-01 20:50:49 +0700

    login done ,remove dropdown

--- src/pages/CreateEventPage.jsx
+++ src/pages/CreateEventPage.jsx
@@ -1,14 +1,12 @@
 /* eslint-disable prettier/prettier */
 // import React from 'react';
 import CreateEventContainer from '../features/auth/components/CreateEventContainer';
-import DropdownList from '../global_components/ListBox';
 // import ListBox from '../global_components/ListBox';
 
 export default function CreateEventPage() {
   return (
     <div className='w-dvw'>
-      {/* <CreateEventContainer /> */}
-      <DropdownList/>
+      <CreateEventContainer />
     </div>
   );
 }
