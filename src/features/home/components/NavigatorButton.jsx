dba4bde1f5ccbd3be0419f194b5cd17f9c6c5b9b
toptaab <top_giveheart@hotmail.com>
2024-03-07 20:30:35 +0700

    set integration button

--- src/features/home/components/NavigatorButton.jsx
+++ src/features/home/components/NavigatorButton.jsx
@@ -1,36 +1,17 @@
-import { MapIcon, CalendarIcon, ExploreIcon } from '../../../icons';
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
-function NavigatorButton({ children }) {
+function NavigatorButton({ children, link }) {
+  const navigate = useNavigate();
   return (
-    <div className='p-4 rounded-2xl flex justify-center items-center bg-primary drop-shadow-[0.1rem_0.2rem_0.1rem_rgba(0,0,0,0.25)]'>
+    <button
+      type='button'
+      onClick={() => navigate(link)}
+      className='p-4 rounded-2xl flex justify-center items-center bg-primary drop-shadow-[0.1rem_0.2rem_0.1rem_rgba(0,0,0,0.25)]'
+    >
       {children}
-    </div>
+    </button>
   );
 }
 
-function NavigatorButtonContainer() {
-  return (
-    <div className='flex justify-evenly md:px-[9.5rem] md:gap-2'>
-      <NavigatorButton className=''>
-        <MapIcon className='w-[2rem] h-[2rem] md:mr-3 lg:mr-4' />
-        <p className='hidden md:block text-white md:text-xl md:font-semibold'>
-          Map
-        </p>
-      </NavigatorButton>
-      <NavigatorButton>
-        <CalendarIcon className='w-[2rem] h-[2rem] md:mr-3 lg:mr-4' />
-        <p className='hidden md:block text-white md:text-xl md:font-semibold'>
-          Calendar
-        </p>
-      </NavigatorButton>
-      <NavigatorButton>
-        <ExploreIcon className='w-[2rem] h-[2rem] md:mr-3 lg:mr-4' />
-        <p className='hidden md:block text-white md:text-xl md:font-semibold'>
-          Explore
-        </p>
-      </NavigatorButton>
-    </div>
-  );
-}
-
-export default NavigatorButtonContainer;
+export default NavigatorButton;
