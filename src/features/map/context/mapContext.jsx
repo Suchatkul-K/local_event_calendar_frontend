0feac2915b3ac2a5bc8278d73683b3f5ad29d437
bankiiz789 <sukkarin7303@gmail.com>
2024-03-07 12:30:11 +0700

    s

--- src/features/map/context/mapContext.jsx
+++ src/features/map/context/mapContext.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { createContext } from 'react';
 import { getAllEventInScope } from '../../../api/event';
+import { useCallback } from 'react';
 
 export const MapContext = createContext();
 
@@ -8,6 +9,10 @@ function MapContextProvider() {
   const [events, setEvents] = useState();
   // Fetch data based on the specified bounds
   const fetchData = (bounds) => getAllEventInScope(bounds); // console.log('Fetching data for bounds:', bounds);
+
+  const fetchDateFn = useCallback(() => {
+    fetchData;
+  }, []);
   return <MapContext.Provider value={{}}></MapContext.Provider>;
 }
 
