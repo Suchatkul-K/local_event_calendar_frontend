de5a76be1e4380fb23e64ed5cf047a627131618c
Suchatkul-K <skempetch@gmail.com>
2024-03-13 14:22:36 +0700

    recode validate and add hide password

--- src/features/auth/components/OrganizerRegisterContainer.jsx
+++ src/features/auth/components/OrganizerRegisterContainer.jsx
@@ -16,21 +16,16 @@ import useAuth from '../hooks/auth';
 import { ORGANIZER } from '../../../constance/index';
 
 export default function OrganizerRegisterContainer() {
-  // const [input, setInput] = useState({
-  //   email: '',
-  //   userName: '',
-  //   password: '',
-  //   confirmPassword: '',
-  //   gender: 'OTHER',
-  //   role: 'ORGANIZER',
-  //   corporation: 'INDIVIDUAL',
-  //   officialName: '',
-  //   companyNumber: '',
-  // });
-  const [input, setInput] = useState({});
+  const [input, setInput] = useState({
+    gender: 'MALE',
+    role: 'ORGANIZER',
+    corporation: 'INDIVIDUAL',
+  });
   const [error, setError] = useState();
   const [profileImage, setProfileImage] = useState(null);
   const [identityCopyImage, setIdentityCopyImage] = useState(null);
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConformPassword, setShowConfirmPassword] = useState(false);
   const { setAuthUser } = useAuth();
 
   const fileEl = useRef(null);
@@ -43,58 +38,51 @@ export default function OrganizerRegisterContainer() {
 
   const handleProfileImage = (e) => {
     setProfileImage(e.target.files[0]);
+    setInput({ ...input, [e.target.name]: e.target.files[0] });
   };
 
   const handleIdentityCopy = (e) => {
     setIdentityCopyImage(e.target.files[0]);
+    setInput({ ...input, [e.target.name]: e.target.files[0] });
   };
 
   const handleProfilePicDelete = () => {
     setProfileImage(null);
+    const temp = { ...input };
+    delete temp.profileImage;
+    setInput(temp);
   };
 
   const handleIdentityCopyPicDelete = () => {
     setIdentityCopyImage(null);
+    const temp = { ...input };
+    delete temp.identityCopyImage;
+    setInput(temp);
   };
 
   const handleSubmit = async (e) => {
     try {
       e.preventDefault();
-      console.log(input);
       const validateResult = validateOrganizerRegister(input);
 
-      if (Object.keys(validateResult).length > 0 || !profileImage) {
+      if (Object.keys(validateResult).length > 0) {
         setError(validateResult);
-        if (!profileImage) {
-          setError((prev) => ({
-            ...prev,
-            profileImage: 'Profile Image is required',
-          }));
-        } else if (!identityCopyImage) {
-          setError((prev) => ({
-            ...prev,
-            identityCopyImage: 'Identity Image is required',
-          }));
-        }
       } else {
         const formData = new FormData();
 
-        formData.append('profileImage', profileImage);
-        formData.append('identityCopyImage', identityCopyImage);
-        formData.append('email', input.email);
-        formData.append('userName', input.userName);
-        formData.append('password', input.password);
-        formData.append('role', 'ORGANIZER');
-        formData.append('gender', input.gender);
-        formData.append('officialName', input.officialName);
-        formData.append('companyNumber', input.companyNumber);
-        formData.append('corporation', input.corporation);
+        Object.entries(input).map((el) => {
+          formData.append(el[0], el[1]);
+          return null;
+        });
+
+        // console.log(...formData);
 
         const registerResult = await apiRegister(formData);
         storeToken(registerResult.data.accessToken);
         const authResult = await authMe(registerResult.data.accessToken);
         setAuthUser(authResult.data);
         setError(null);
+        console.log('Organizer register success');
         navigate('/home');
       }
     } catch (err) {
@@ -103,80 +91,99 @@ export default function OrganizerRegisterContainer() {
   };
 
   useEffect(() => window.scrollTo(0, 0), []);
-
   return (
     <form onSubmit={handleSubmit}>
-      <div className='py-12 '>
-        <div className=' mx-auto flex flex-col  gap-[2rem] w-full p-[3rem]'>
-          <div className='text-[1.75rem] font-semibold'>Create An Account</div>
-          <div className='flex flex-col items-center'>
-            {profileImage ? (
-              <div className=' relative'>
-                <img
-                  src={URL.createObjectURL(profileImage)}
-                  alt='event'
-                  className='w-[200px] h-[200px] object-cover'
-                />
-                <button
-                  type='button'
-                  className='absolute top-0 right-0 m-3 bg-white w-[1.5rem] font-bold h-[1.5rem] text-center rounded-[100%]'
-                  onClick={handleProfilePicDelete}
-                >
-                  X
-                </button>
-              </div>
-            ) : (
-              <PictureIcon />
-            )}
-          </div>
-          <div className='flex flex-row justify-end'>
-            <div className='flex flex-col items-end'>
-              <div className=' sm:[30%] flex flex-col '>
-                <input
-                  type='file'
-                  ref={fileEl}
-                  className='hidden'
-                  onChange={handleProfileImage}
-                />
-                <Button onClick={() => fileEl.current.click()}>
-                  Upload Profile
-                </Button>
-              </div>
-              <div>
-                {error?.profileImage ? (
-                  <div className='text-red-500 pl-[0.5rem]'>
-                    Official Profile is required
-                  </div>
-                ) : null}
-              </div>
+      <div className=' mx-auto flex flex-col  gap-[2rem] w-full p-[3rem]'>
+        <div className='text-[1.75rem] font-semibold text-center'>
+          Create An Account
+        </div>
+
+        {/* profile image */}
+        <div className='flex flex-col items-center'>
+          {profileImage ? (
+            <div className=' relative'>
+              <img
+                src={URL.createObjectURL(profileImage)}
+                alt='event'
+                className='w-[200px] h-[200px] object-cover'
+              />
+              <button
+                type='button'
+                className='absolute top-0 right-0 m-3 bg-white w-[1.5rem] font-bold h-[1.5rem] text-center rounded-[100%]'
+                onClick={handleProfilePicDelete}
+              >
+                X
+              </button>
+            </div>
+          ) : (
+            <PictureIcon />
+          )}
+        </div>
+
+        {/* upload profile */}
+        <div className='flex flex-row justify-end'>
+          <div className='flex flex-col items-end'>
+            <div className=' sm:[30%] flex flex-col '>
+              <input
+                type='file'
+                ref={fileEl}
+                className='hidden'
+                name='profileImage'
+                onChange={handleProfileImage}
+              />
+              <Button onClick={() => fileEl.current.click()}>
+                Upload Profile
+              </Button>
+            </div>
+            <div>
+              {error?.profileImage ? (
+                <div className='text-red-500 pl-[0.5rem]'>
+                  {error?.profileImage}
+                </div>
+              ) : null}
             </div>
           </div>
+        </div>
 
-          {/* email */}
+        {/* email */}
+        <Input
+          name='email'
+          placeholder='Example@gmail.com'
+          value={input}
+          onChange={handleChange}
+          title='Email'
+          errorMessage={error?.email}
+        >
+          <EmailIcon />
+        </Input>
+
+        {/* username */}
+        <Input
+          name='userName'
+          placeholder='Username'
+          value={input}
+          onChange={handleChange}
+          title='Username'
+          errorMessage={error?.userName}
+        >
+          <ProfileIcon />
+        </Input>
+
+        {/* password */}
+        {showPassword ? (
           <Input
-            name='email'
-            placeholder='Example@gmail.com'
+            name='password'
+            placeholder='Password'
             value={input}
             onChange={handleChange}
-            title='Email'
-            errorMessage={error?.email}
+            title='Password'
+            errorMessage={error?.password}
+            type='text'
+            onClickButton={() => setShowPassword(false)}
           >
-            <EmailIcon />
+            <LockerIcon />
           </Input>
-
-          {/* username */}
-          <Input
-            name='userName'
-            placeholder='Username'
-            value={input}
-            onChange={handleChange}
-            title='Username'
-            errorMessage={error?.userName}
-          >
-            <ProfileIcon />
-          </Input>
-
-          {/* password */}
+        ) : (
           <Input
             name='password'
             placeholder='Password'
@@ -185,142 +192,151 @@ export default function OrganizerRegisterContainer() {
             title='Password'
             errorMessage={error?.password}
             type='password'
+            onClickButton={() => setShowPassword(true)}
           >
             <LockerIcon />
           </Input>
+        )}
 
-          {/* confirm password */}
+        {/* confirm password */}
+        {showConformPassword ? (
           <Input
             name='confirmPassword'
             placeholder='Confirm password'
             value={input}
             onChange={handleChange}
-            title='confirmPassword'
+            title='Confirm password'
             errorMessage={error?.confirmPassword}
-            type='password'
+            type='text'
+            onClickButton={() => setShowConfirmPassword(false)}
           >
             <LockerIcon />
           </Input>
-
-          <div className='flex flex-row justify-between'>
-            {/* Select gender */}
-            <div className='flex flex-col'>
-              <div>Gender</div>
-              <select
-                name='gender'
-                className='h-10  rounded-lg'
-                onChange={handleChange}
-                defaultValue='MALE'
-              >
-                <option value='MALE'>MALE</option>
-                <option value='FEMALE'>FEMALE</option>
-                <option value='OTHER'>OTHER</option>
-              </select>
-            </div>
-          </div>
-
-          {/* id copy image */}
-          <div className='flex flex-col items-center'>
-            {identityCopyImage ? (
-              <div className='relative'>
-                <img
-                  src={URL.createObjectURL(identityCopyImage)}
-                  alt='event'
-                  className='w-[200px] h-[200px] object-cover'
-                />
-                <button
-                  type='button'
-                  className='absolute top-0 right-0 m-3 bg-white w-[1.5rem] font-bold h-[1.5rem] text-center rounded-[100%]'
-                  onClick={handleIdentityCopyPicDelete}
-                >
-                  X
-                </button>
-              </div>
-            ) : (
-              <IdentityCardIcon />
-            )}
-          </div>
-          <div className='flex flex-row justify-end '>
-            <div className='flex flex-col items-end'>
-              <div className='sm:[30%]'>
-                <input
-                  type='file'
-                  ref={fileEl2}
-                  className='hidden'
-                  onChange={handleIdentityCopy}
-                />
-                <Button onClick={() => fileEl2.current.click()}>
-                  Upload ID
-                </Button>
-              </div>
-              <div>
-                {error?.profileImage ? (
-                  <div className='text-red-500 pl-[0.5rem]'>
-                    ID Copy is required
-                  </div>
-                ) : null}
-              </div>
-            </div>
-          </div>
-
-          {/* official name */}
+        ) : (
           <Input
-            name='officialName'
-            placeholder='Official name'
+            name='confirmPassword'
+            placeholder='Confirm password'
             value={input}
             onChange={handleChange}
-            title='Official Name'
-            errorMessage={error?.officialName}
-          />
+            title='Confirm password'
+            errorMessage={error?.confirmPassword}
+            type='password'
+            onClickButton={() => setShowConfirmPassword(true)}
+          >
+            <LockerIcon />
+          </Input>
+        )}
 
-          <div className='flex flex-col gap-4'>
-            <div>Please select your entity type:</div>
+        {/* Select gender */}
+        <div className='flex flex-row justify-between'>
+          <div className='flex flex-col'>
+            <div>Gender</div>
             <select
-              name='corporation'
+              name='gender'
               className='h-10  rounded-lg'
               onChange={handleChange}
-              defaultValue={ORGANIZER.INDIVIDUAL}
+              value={input.gender}
             >
-              <option value={ORGANIZER.INDIVIDUAL}>Individual</option>
-              <option value={ORGANIZER.CORPORATION}>Corporation</option>
+              <option value='MALE'>MALE</option>
+              <option value='FEMALE'>FEMALE</option>
+              <option value='OTHER'>OTHER</option>
             </select>
-            {/* company number */}
-            {input?.corporation === ORGANIZER.CORPORATION && (
-              <Input
-                name='companyNumber'
-                placeholder='Company number'
-                value={input}
-                onChange={handleChange}
-                title='Company Number'
-                errorMessage={error?.companyNumber}
-              />
-            )}
           </div>
+        </div>
 
-          {/* button group */}
-          <div className=' mx-auto flex flex-col justify-center text-center gap-[1.5rem] space-between w-full'>
-            <button
-              type='submit'
-              className='btn bg-primary h-12 text-white text-[1rem] '
-            >
-              Create Account
-            </button>
-
-            <hr className='my-2 border border-gray-300' />
-
-            <button
-              type='button'
-              className='btn bg-Line h-12 text-white text-[1rem]'
-            >
-              Continue with Line
-            </button>
-
-            <div className='text-[1rem]'>
-              <span>Already Have An Account ?</span>
-              <Link to='/login'>
-                <span className='text-green-700'> Log in</span>
-              </Link>
+        {/* id copy image */}
+        <div className='flex flex-col items-center'>
+          {identityCopyImage ? (
+            <div className='relative'>
+              <img
+                src={URL.createObjectURL(identityCopyImage)}
+                alt='event'
+                className='w-[200px] h-[200px] object-cover'
+              />
+              <button
+                type='button'
+                className='absolute top-0 right-0 m-3 bg-white w-[1.5rem] font-bold h-[1.5rem] text-center rounded-[100%]'
+                onClick={handleIdentityCopyPicDelete}
+              >
+                X
+              </button>
             </div>
+          ) : (
+            <IdentityCardIcon />
+          )}
+        </div>
+        <div className='flex flex-row justify-end '>
+          <div className='flex flex-col items-end'>
+            <div className='sm:[30%]'>
+              <input
+                type='file'
+                ref={fileEl2}
+                className='hidden'
+                name='identityCopyImage'
+                onChange={handleIdentityCopy}
+              />
+              <Button onClick={() => fileEl2.current.click()}>Upload ID</Button>
+            </div>
+            <div>
+              {error?.identityCopyImage ? (
+                <div className='text-red-500 pl-[0.5rem]'>
+                  {error?.identityCopyImage}
+                </div>
+              ) : null}
+            </div>
+          </div>
+        </div>
+
+        {/* official name */}
+        <Input
+          name='officialName'
+          placeholder='Official name'
+          value={input}
+          onChange={handleChange}
+          title='Official Name'
+          errorMessage={error?.officialName}
+        />
+
+        {/* corporation type */}
+        <div className='flex flex-col gap-4'>
+          <div>Please select your entity type:</div>
+          <select
+            name='corporation'
+            className='h-10  rounded-lg'
+            onChange={handleChange}
+            value={input.corporation}
+          >
+            <option value={ORGANIZER.INDIVIDUAL}>Individual</option>
+            <option value={ORGANIZER.CORPORATION}>Corporation</option>
+          </select>
+
+          {/* company number */}
+          {input?.corporation === ORGANIZER.CORPORATION && (
+            <Input
+              name='companyNumber'
+              placeholder='Company number'
+              value={input}
+              onChange={handleChange}
+              title='Company Number'
+              errorMessage={error?.companyNumber}
+            />
+          )}
+        </div>
+
+        {/* button group */}
+        <div className=' mx-auto flex flex-col justify-center text-center gap-[1rem] space-between w-full'>
+          <button
+            type='submit'
+            className='btn bg-primary h-12 text-white text-[1rem] '
+          >
+            Create Account
+          </button>
+
+          <div className='text-[1rem]'>
+            <span>Already Have An Account ?</span>
+            <Link to='/login'>
+              <span className='text-green-700'> Log in</span>
+            </Link>
           </div>
         </div>
       </div>
--- src/features/auth/validation/validate-register.js
+++ src/features/auth/validation/validate-register.js
@@ -1,6 +1,12 @@
 import Joi from 'joi';
 
 const organizerRegisterSchema = Joi.object({
+  profileImage: Joi.any().required().disallow('empty').messages({
+    'any.required': 'Official image is required',
+  }),
+  identityCopyImage: Joi.any().required().disallow('empty').messages({
+    'any.required': 'ID copy image is required',
+  }),
   email: Joi.string()
     .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } })
     .required()
@@ -21,7 +27,7 @@ const organizerRegisterSchema = Joi.object({
   confirmPassword: Joi.string().valid(Joi.ref('password')).required().messages({
     'any.required': 'Confirm password is required',
     'string.empty': 'Confirm password is required',
-    'any.only': 'Password and confirm password should be matched ',
+    'any.only': 'Confirm password should be matched',
   }),
   gender: Joi.string().required().trim().messages({
     'any.required': 'Gender is required',
@@ -35,37 +41,39 @@ const organizerRegisterSchema = Joi.object({
     'any.required': 'Official name is required',
     'string.empty': 'Official Name is required',
   }),
-});
+}).unknown(true);
 
 const userRegisterSchema = Joi.object({
+  profileImage: Joi.any().required().disallow('empty').messages({
+    'any.required': 'Official image is required',
+  }),
   email: Joi.string()
     .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } })
     .required()
     .trim()
     .messages({
+      'any.required': 'Email is required',
       'string.empty': 'Email is required',
       'string.email': 'Email should be in email format',
     }),
-  userName: Joi.string()
-    .required()
-    .trim()
-    .messages({ 'string.empty': 'Username is required' }),
-  password: Joi.string()
-    .required()
-    .messages({ 'string.empty': 'Password is required' }),
-  confirmPassword: Joi.string().valid(Joi.ref('password')).required().messages({
-    'string.empty': ' Confirm password is required',
-    'any.only': 'Password and confirm password should be matched ',
+  userName: Joi.string().required().trim().messages({
+    'any.required': 'Username is required',
+    'string.empty': 'Username is required',
   }),
-  gender: Joi.string()
-    .required()
-    .trim()
-    .messages({ 'string.empty': 'gender is required' }),
-  role: Joi.string()
-    .required()
-    .trim()
-    .messages({ 'string.empty': 'role is required' }),
-});
+  password: Joi.string().required().messages({
+    'any.required': 'Password is required',
+    'string.empty': 'Password is required',
+  }),
+  confirmPassword: Joi.string().valid(Joi.ref('password')).required().messages({
+    'any.required': 'Confirm password is required',
+    'string.empty': 'Confirm password is required',
+    'any.only': 'Confirm password should be matched',
+  }),
+  gender: Joi.string().required().trim().messages({
+    'any.required': 'Gender is required',
+    'string.empty': 'Gender is required',
+  }),
+}).unknown(true);
 
 export const validateOrganizerRegister = (input) => {
   const { error } = organizerRegisterSchema.validate(input, {
@@ -84,16 +92,13 @@ export const validateOrganizerRegister = (input) => {
 
 export const validateUserRegister = (input) => {
   const { error } = userRegisterSchema.validate(input, { abortEarly: false });
+  console.dir(error);
 
   const errorObject = {};
-  const temp = error?.details.map((el) => {
+  error?.details.map((el) => {
     errorObject[el.path[0]] = el.message;
     return null;
   });
-  console.log('error object is here!!!!');
-  console.log(temp);
 
   return errorObject;
 };
-
-export const temp = () => {};
